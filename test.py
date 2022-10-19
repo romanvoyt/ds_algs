c41e764043f54b4d203c4336474eb21516dd97ba
romanvoyt <romanvoitetckii@gmail.com>
2022-10-19 14:25:31 +0300

    Test file is updated. Selection sort testing is added.

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ import random
 from algorithms.binary_search import Algo
 from algorithms.selection_sort import SelectionSort
 
+
 def test_binary_search():
     numbers = [random.randint(1, 10) for _ in range(20)]
     numbers.sort()
     algo = Algo(numbers, 3)
     index = algo.binary_search()
     print(f'{index}')
+
 
 def test_selection_sort():
     numbers = [random.randint(1, 30) for _ in range(10)]
