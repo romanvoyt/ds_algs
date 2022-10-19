20b91ebbc8c0144b5dc36bcb3b175baad8246798
romanvoyt <romanvoitetckii@gmail.com>
2022-10-19 13:23:07 +0300

    binary_search.py is updated. OOP

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,1 +1,1 @@
-from algorithms import binary_search
+
--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,14 +1,19 @@
-def binary_search(list, item):
-    low = 0
-    high = len(list)
+class Algo:
+    def __init__(self, array: list, item: int):
+        self.array = array
+        self.item = item
 
-    while low <= high:
-        mid = (low + high)/2
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
+    def binary_search(self):
+        low = 0
+        high = len(self.array)
+
+        while low <= high:
+            mid = int((low + high)/2)
+            guess = self.array[mid]
+            if guess == self.item:
+                return mid
+            if guess > self.item:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return None
