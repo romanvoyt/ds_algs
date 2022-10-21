42f26507a38a17835cff300cc4323e2d9109c0c0
romanvoyt <romanvoitetckii@gmail.com>
2022-10-21 13:04:55 +0300

    test.py is updated. New test for quick sort is added

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import random
 
-from algorithms.binary_search import Algo
+from algorithms.binary_search import BinarySearch
 from algorithms.selection_sort import SelectionSort
+from algorithms.quick_sort import QuickSort
 
 
 def test_binary_search():
     numbers = [random.randint(1, 10) for _ in range(20)]
     numbers.sort()
-    algo = Algo(numbers, 3)
-    index = algo.binary_search()
+    algo = BinarySearch(numbers, 3)
+    index = algo.search()
     print(f'{index}')
 
 
@@ -19,6 +20,15 @@ def test_selection_sort():
     sorted_array = selection_sort.sort()
     print(f'sortee array: {sorted_array}')
 
+# The sorting is deleting duplicates, to fix
+def test_quick_sort():
+    numbers = [random.randint(1, 33) for _ in range(100)]
+    print(f'unsorted array {numbers}')
+    quickie = QuickSort()
+    quickie.array = numbers
+    sorted_array = quickie.sort(quickie.array)
+    print(f'sorted array: {sorted_array}')
+
 
 if __name__ == '__main__':
-    test_selection_sort()
+    test_quick_sort()
