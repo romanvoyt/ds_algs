42e51e64609b283444cd8eaf9f90c0c4b3649e51
romanvoyt <romanvoitetckii@gmail.com>
2022-10-21 12:53:13 +0300

    Quick Sort has been done

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 class QuickSort:
     def __int__(self, array: List[list]):
         self.array = array
@@ -7,12 +8,22 @@ class QuickSort:
         temp = a
         a = b
         b = temp
-        return a, b
 
-    def sort(self) -> List[list]:
-        if len(self.array) < 2:
-            return self.array
+    # To do: fix bug (during the sorting the repeatable elements of the list are deleted)
 
-        base_value_id = int(len(self.array)/2)
-        if self.array[0] > self.array[base_value_id]:
-            self.array[0], self.array[base_value_id] = self.swap(self.array[0], self.array[base_value_id])
+    def sort(self, array):
+        if len(array) < 2:
+            return array
+        base = array[0]
+        lower_subarray = [num for num in array[1:] if num < base]
+        higher_subarray = [num for num in array[1:] if num > base]
+        return self.sort(lower_subarray) + [base] + self.sort(higher_subarray)
+
+
+if __name__ == '__main__':
+    numbers = [random.randint(1, 20) for _ in range(10)]
+    print(f'unsorted array : {numbers}')
+    quickie = QuickSort()
+    quickie.array = numbers
+    array = quickie.sort(quickie.array)
+    print(f'sorted array {array}')
